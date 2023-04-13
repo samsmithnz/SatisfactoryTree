18198e90f45a932327c8e10b7798fdb8fd48e839
Sam Smith <samsmithnz@gmail.com>
2023-04-13 11:07:19 -0400

    Added new buildings

--- src/SatisfactoryTree/Helpers/BuildingPool.cs
+++ src/SatisfactoryTree/Helpers/BuildingPool.cs
@@ -4,25 +4,25 @@ namespace SatisfactoryTree.Helpers
 {
     public class BuildingPool
     {
-        public static Item MiningMachineMk1()
+        public static Building MiningMachineMk1()
         {
-            return new Item(1, "Iron Ore",
-                "Iron_Ore.webp",
-                ItemType.Item,
-                ResearchType.Tier1)
-            {
-                Recipes =
-                {
-                    new Recipe(1,
-                        60,
-                        new(),
-                        new()
-                        {
-                            { "Iron Ore", 1 }
-                        },
-                        ManufactoringBuildingType.MiningMachine)
-                }
-            };
+            return new Building("Mining Machine",
+                "MinerMk3_256.png",
+                ManufactoringBuildingType.MiningMachine);
+        }
+
+        public static Building AssemblerMk1()
+        {
+            return new Building("Assembler",
+                "AssemblerMk1_256.png",
+                ManufactoringBuildingType.Assembler);
+        }
+
+        public static Building ConstructorMk1()
+        {
+            return new Building("Constructor",
+                "ConstructorMk1_256.png",
+                ManufactoringBuildingType.Constructor);
         }
     }
 }
--- src/SatisfactoryTree/Models/Building.cs
+++ src/SatisfactoryTree/Models/Building.cs
@@ -1,9 +1,18 @@
-﻿using SatisfactoryTree.Models;
-
-namespace SatisfactoryTree.Models
+﻿namespace SatisfactoryTree.Models
 {
     public class Building
     {
+        public Building(string name,
+            string image,
+            ManufactoringBuildingType buildingType)
+        {
+            Name = name; ;
+            Image = image;
+            BuildingType = buildingType;
+        }
+
+        public string Name { get; set; }
+        public string Image { get; set; }
         public ManufactoringBuildingType BuildingType { get; set; }
     }
 }
