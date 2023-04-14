63cc7d7d9046529f7855b1f731bbd97cee7805f9
Sam Smith <samsmithnz@gmail.com>
2023-04-14 17:46:19 -0400

    Added production items and links

---
+++ src/SatisfactoryTree/Models/ProductionItem.cs
@@ -0,0 +1,15 @@
+﻿namespace SatisfactoryTree.Models
+{
+    public class ProductionItem
+    {
+        public ProductionItem(Item item, decimal quantity)
+        {
+            Item = item;
+            Quantity = quantity;
+            Parents = new();
+        }
+        public Item Item { get; set; }
+        public decimal Quantity { get; set; }
+        public List<ProductionItem> Parents { get; set; }
+    }
+}
