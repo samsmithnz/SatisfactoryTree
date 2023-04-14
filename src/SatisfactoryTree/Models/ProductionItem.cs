234b42e4c0c96d2efc43d31468dc63d8b5422e18
Sam Smith <samsmithnz@gmail.com>
2023-04-14 17:48:04 -0400

    added building to production item

--- src/SatisfactoryTree/Models/ProductionItem.cs
+++ src/SatisfactoryTree/Models/ProductionItem.cs
@@ -2,13 +2,15 @@
 {
     public class ProductionItem
     {
-        public ProductionItem(Item item, decimal quantity)
+        public ProductionItem(Item? item, Building? building, decimal quantity)
         {
             Item = item;
+            Building = building;
             Quantity = quantity;
             Parents = new();
         }
-        public Item Item { get; set; }
+        public Item? Item { get; set; }
+        public Building? Building { get; set; }
         public decimal Quantity { get; set; }
         public List<ProductionItem> Parents { get; set; }
     }
