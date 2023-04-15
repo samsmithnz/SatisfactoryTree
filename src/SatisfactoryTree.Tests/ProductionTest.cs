65fdeae811a375d9adabd3cfa85d2edc21878f56
Sam Smith <samsmithnz@gmail.com>
2023-04-14 20:58:05 -0400

    code gardening

--- src/SatisfactoryTree.Tests/ProductionTest.cs
+++ src/SatisfactoryTree.Tests/ProductionTest.cs
@@ -24,15 +24,12 @@ namespace SatisfactoryTree.Tests
             }
 
             //Assert
-            //1 Copper Ore -> Copper Ingot
             Assert.IsNotNull(startingItem);
             Assert.AreEqual(2, results.Count);
             Assert.IsNotNull(results[0].Item);
             Assert.AreEqual(30, results[0].Quantity);
             Assert.AreEqual("Copper Ore", results[1].Item?.Name);
             Assert.AreEqual(30, results[1].Quantity);
-            //Assert.IsTrue(rawMaterials.ContainsKey("Copper Ore"));
-            //Assert.AreEqual(1, rawMaterials["Copper Ore"]);
         }
 
 
@@ -59,8 +56,6 @@ namespace SatisfactoryTree.Tests
             Assert.AreEqual(30, results[0].Quantity);
             Assert.AreEqual("Iron Ingot", results[1].Item?.Name);
             Assert.AreEqual(45, results[1].Quantity);
-            //Assert.IsTrue(rawMaterials.ContainsKey("Copper Ore"));
-            //Assert.AreEqual(1, rawMaterials["Copper Ore"]);
         }
     }
 }
