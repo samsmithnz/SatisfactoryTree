d3ea9948632f00191008fdabc89c0d7b65717f77
Sam Smith <samsmithnz@gmail.com>
2023-04-16 11:10:01 -0400

    reworking production calculator

--- src/SatisfactoryTree/Models/ProductionItem.cs
+++ src/SatisfactoryTree/Models/ProductionItem.cs
@@ -2,17 +2,19 @@
 {
     public class ProductionItem
     {
-        public ProductionItem(Item? item, decimal quantity,
-             Building? building = null)
+        public ProductionItem(Item? item, decimal quantity)
+        //Building? building, decimal buildingQuantity)
         {
             Item = item;
-            Building = building;
             Quantity = quantity;
-            Parents = new();
+            //Building = building;
+            //BuildingQuantity = buildingQuantity;
+            Dependencies = new();
         }
         public Item? Item { get; set; }
-        public Building? Building { get; set; }
         public decimal Quantity { get; set; }
-        public List<ProductionItem> Parents { get; set; }
+        //public Building? Building { get; set; }
+        //public decimal BuildingQuantity { get; set; }
+        public Dictionary<string, decimal> Dependencies { get; set; }
     }
 }
--- src/SatisfactoryTree/Models/ProductionLink.cs
+++ src/SatisfactoryTree/Models/ProductionLink.cs
@@ -1,14 +1,14 @@
-﻿namespace SatisfactoryTree.Models
-{
-    public class ProductionLink
-    {
-        public ProductionLink(string name, decimal quantity)
-        {
-            Name = name;
-            Quantity = quantity;
-        }
+﻿//namespace SatisfactoryTree.Models
+//{
+//    public class ProductionLink
+//    {
+//        public ProductionLink(string name, decimal quantity)
+//        {
+//            Name = name;
+//            Quantity = quantity;
+//        }
 
-        public string Name { get; set; }
-        public decimal Quantity { get; set; }
-    }
-}
+//        public string Name { get; set; }
+//        public decimal Quantity { get; set; }
+//    }
+//}
