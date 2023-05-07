1ceed20c342e5d38673547c848c99f481b63eb01
Sam Smith <samsmithnz@gmail.com>
2023-05-07 12:15:23 -0400

    updated test

--- src/SatisfactoryTree.Tests/ProductionTest.cs
+++ src/SatisfactoryTree.Tests/ProductionTest.cs
@@ -59,7 +59,6 @@ namespace SatisfactoryTree.Tests
 
         }
 
-
         [TestMethod]
         public void IronPlateProductionTest()
         {
@@ -87,5 +86,34 @@ namespace SatisfactoryTree.Tests
             Assert.AreEqual(45, results[1].Quantity);
             Assert.AreEqual(1.5M, results[1].BuildingQuantityRequired);
         }
+
+
+        //[TestMethod]
+        //public void ReinforcedIronPlateProductionTest()
+        //{
+        //    //Arrange
+        //    SatisfactoryProduction graph = new(); 
+        //    string itemName = "Reinforced Iron Plate";
+        //    decimal quantity = 30;
+        //    ProductionItem? startingItem = new(graph.FindItem(itemName), quantity);
+        //    List<ProductionItem> results = new();
+
+        //    //Act
+        //    if (startingItem != null)
+        //    {
+        //        results = graph.BuildSatisfactoryProductionPlan(startingItem);
+        //    }
+
+        //    //Assert
+        //    Assert.IsNotNull(startingItem);
+        //    //Assert.AreEqual(3, results.Count);
+        //    //Assert.IsNotNull(results[0].Item);
+        //    //Assert.AreEqual("Iron Plate", results[0].Item?.Name);
+        //    //Assert.AreEqual(30, results[0].Quantity); 
+        //    //Assert.AreEqual(1.5M, results[0].BuildingQuantityRequired);
+        //    //Assert.AreEqual("Iron Ingot", results[1].Item?.Name);
+        //    //Assert.AreEqual(45, results[1].Quantity);
+        //    //Assert.AreEqual(1.5M, results[1].BuildingQuantityRequired);
+        //}
     }
 }
