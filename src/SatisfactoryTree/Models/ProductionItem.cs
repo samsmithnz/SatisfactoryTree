b9dc77b5cce5d89c267098903c184120a3343323
Sam Smith <samsmithnz@gmail.com>
2023-05-09 22:12:21 -0400

    Updated class

--- src/SatisfactoryTree/Models/ProductionItem.cs
+++ src/SatisfactoryTree/Models/ProductionItem.cs
@@ -3,19 +3,14 @@
     public class ProductionItem
     {
         public ProductionItem(Item? item, decimal quantity)
-        //Building? building, decimal buildingQuantity)
         {
             Item = item;
             Quantity = quantity;
-            //Building = building;
-            //BuildingQuantity = buildingQuantity;
             Dependencies = new();
         }
         public Item? Item { get; set; }
         public decimal Quantity { get; set; }
         public decimal BuildingQuantityRequired { get; set; }
-        //public Building? Building { get; set; }
-        //public decimal BuildingQuantity { get; set; }
         public Dictionary<string, decimal> Dependencies { get; set; }
     }
 }
