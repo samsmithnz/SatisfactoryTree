694f38ed7cc4a2e42247dc3066315b3004234fd4
Sam Smith <samsmithnz@gmail.com>
2023-06-25 21:54:24 -0400

    Added power to building object

--- src/SatisfactoryTree/Models/Building.cs
+++ src/SatisfactoryTree/Models/Building.cs
@@ -4,15 +4,18 @@
     {
         public Building(string name,
             string image,
-            ManufactoringBuildingType buildingType)
+            ManufactoringBuildingType buildingType,
+            decimal powerConsumption)
         {
             Name = name; ;
             Image = image;
             BuildingType = buildingType;
+            PowerConsumption = powerConsumption;
         }
 
         public string Name { get; set; }
         public string Image { get; set; }
-        public ManufactoringBuildingType BuildingType { get; set; }
+        public ManufactoringBuildingType BuildingType { get; set; };
+        public decimal PowerConsumption { get; set; }
     }
 }
