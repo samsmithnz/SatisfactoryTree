a29de872d70c4fc9f1b7e87d9f10e5d315c1d891
Sam Smith <samsmithnz@gmail.com>
2023-06-27 15:16:37 -0400

    Added production calculation class

---
+++ src/SatisfactoryTree/Models/ProductionCalculation.cs
@@ -0,0 +1,13 @@
+﻿namespace SatisfactoryTree.Models
+{
+    public class ProductionCalculation
+    {
+        public ProductionCalculation()
+        {
+            ProductionItems = new();
+        }
+
+        public List<ProductionItem> ProductionItems { get; set; }
+        public decimal PowerConsumption { get; set; }
+    }
+}
