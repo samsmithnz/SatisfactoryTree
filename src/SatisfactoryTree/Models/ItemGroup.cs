01ef4373ff865c920083a4ed7e5b628ae5c7d75c
Sam Smith <samsmithnz@gmail.com>
2023-09-22 14:27:23 -0400

    Added new item group class

---
+++ src/SatisfactoryTree/Models/ItemGroup.cs
@@ -0,0 +1,9 @@
+﻿namespace SatisfactoryTree.Models
+{
+    public class ItemGroup
+    {
+        public int Level { get; set; }
+        public string Name { get; set; }
+        public int ResearchTier { get; set; }
+    }
+}
