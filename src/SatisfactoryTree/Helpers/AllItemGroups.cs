00c68656b98204ea0cc0963629de0fbea228470c
Sam Smith <samsmithnz@gmail.com>
2023-09-22 14:29:00 -0400

    Added base class of new helper

---
+++ src/SatisfactoryTree/Helpers/AllItemGroups.cs
@@ -0,0 +1,17 @@
+﻿using SatisfactoryTree.Models;
+
+namespace SatisfactoryTree.Helpers
+{
+    public class AllItemGroups
+    {
+        List<ItemGroup> ItemGroups { get; set; }
+        public AllItemGroups()
+        {
+            List<ItemGroup> itemGroups = new();
+
+
+            ItemGroups = itemGroups;
+
+        }
+    }
+}
