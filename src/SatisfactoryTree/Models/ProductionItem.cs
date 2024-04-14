20d22a1e37f3655863194ea37b8ed93b05a29df5
Sam Smith <samsmithnz@gmail.com>
2024-04-14 17:00:48 -0400

    Added building to production calculation

--- src/SatisfactoryTree/Models/ProductionItem.cs
+++ src/SatisfactoryTree/Models/ProductionItem.cs
@@ -1,22 +1,36 @@
-﻿namespace SatisfactoryTree.Models
+﻿using SatisfactoryTree.Helpers;
+
+namespace SatisfactoryTree.Models
 {
     public class ProductionItem
     {
         public ProductionItem(Item? item, decimal quantity)
         {
             Item = item;
+            if (item != null)
+            {
+                Building = AllBuildings.FindBuilding(item.Recipes[0].Building);
+            }
             Quantity = quantity;
-            Dependencies = new();
+            Dependencies = [];
         }
         public Item? Item { get; set; }
         public decimal Quantity { get; set; }
         public decimal BuildingQuantityRequired { get; set; }
+        public Building? Building { get; set; }
         public Dictionary<string, decimal> Dependencies { get; set; }
         public bool OutputItem { get; set; }
         public string Name => Item?.Name ?? "Unknown";
         public override string ToString()
         {
-            return $"{Name} x{Quantity}";
+            if (Building != null)
+            {Building to
+                return $"{Name} x{Quantity} ({Building.Name} x{BuildingQuantityRequired})";
+            }
+            else
+            {
+                return $"{Name} x{Quantity}";
+            }
         }
     }
 }
