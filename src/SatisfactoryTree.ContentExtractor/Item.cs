29ae1046eed2615d2079c5097ed86502a01aa8a5
Sam Smith <samsmithnz@gmail.com>
2024-10-10 15:01:30 -0400

    Added new extraction code

---
+++ src/SatisfactoryTree.ContentExtractor/Item.cs
@@ -0,0 +1,15 @@
+﻿namespace SatisfactoryTree.ContentExtractor
+{
+    public class Item(string? className, string? displayName, string? description, string? stackSize, string? fluidColor, string? resourceSinkPoints)
+    {
+        //Common properties
+        public string? ClassName { get; set; } = className;
+        public string? DisplayName { get; set; } = displayName;
+        public string? Description { get; set; } = description;
+
+        //Item properties
+        public string? StackSize { get; set; } = stackSize;
+        public string? FluidColor { get; set; } = fluidColor;
+        public string? ResourceSinkPoints { get; set; } = resourceSinkPoints;
+    }
+}
