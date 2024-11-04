7cfd16e5d4f82f6186a29b05a448d690ea04b5f9
Sam Smith <samsmithnz@gmail.com>
2024-11-03 19:34:29 -0500

    updated production

--- src/SatisfactoryTree/SatisfactoryProduction.cs
+++ src/SatisfactoryTree/SatisfactoryProduction.cs
@@ -23,23 +23,173 @@ namespace SatisfactoryTree
 
         public ProductionCalculation NewBuildProductionPlan(NewTargetItem newTargetItem)
         {
-            ProductionItems = [];
-            ProductionItem productionItem = new(newTargetItem.ItemName, newTargetItem.ItemQuantity);
-            NewRecipe? recipe = FindRecipe(newTargetItem.ItemName);
-            if (recipe != null)
+            if (newTargetItem != null)
             {
-                decimal ratePerMinute = 60M / recipe.ManufactoringDuration;
-                decimal buildingQuantityRequired = newTargetItem.ItemQuantity / ratePerMinute;
-                productionItem.BuildingQuantityRequired = buildingQuantityRequired;
+                ProcessOutputItem(newTargetItem);
+
+                //Search for items that are not dependencies to identify outputs
+                List<string> dependencies = [];
+                foreach (ProductionItem item in ProductionItems)
+                {
+                    foreach (KeyValuePair<string, decimal> dependent in item.Dependencies)
+                    {
+                        if (!dependencies.Any(p => p == dependent.Key))
+                        {
+                            dependencies.Add(dependent.Key);
+                        }
+                    }
+                }
+                //Mark items that are not dependencies
+                foreach (ProductionItem item in ProductionItems)
+                {
+                    if (item != null && item.Item != null)
+                    {
+                        if (!dependencies.Any(p => p == item.Item?.Name))
+                        {
+                            item.OutputItem = true;
+                        }
+                    }
+                }
             }
             ProductionCalculation productionCalculation = new()
             {
                 ProductionItems = ProductionItems,
-                PowerConsumption = 7.5M
+                PowerConsumption = PowerConsumption
             };
-            ProductionItems.Add(productionItem);
-
             return productionCalculation;
+
+
+            //ProductionItems = [];
+            //ProductionItem productionItem = new(newTargetItem.ItemName, newTargetItem.ItemQuantity);
+            //NewRecipe? recipe = FindRecipe(newTargetItem.ItemName);
+            //if (recipe != null)
+            //{
+            //    decimal ratePerMinute = 60M / recipe.ManufactoringDuration;
+            //    decimal buildingQuantityRequired = newTargetItem.ItemQuantity / ratePerMinute;
+            //    productionItem.BuildingQuantityRequired = buildingQuantityRequired;
+            //}
+            //ProductionCalculation productionCalculation = new()
+            //{
+            //    ProductionItems = ProductionItems,
+            //    PowerConsumption = 7.5M
+            //};
+            //ProductionItems.Add(productionItem);
+
+            //return productionCalculation;
+        }
+
+        //Taking an output item, find the inputs required to produce it
+        private bool ProcessOutputItem(NewTargetItem targetItem)
+        {
+            List<KeyValuePair<string, decimal>> inputs = [];
+            ProductionItem? currentItemMatch = null;
+            if (targetItem != null)
+            {
+                //Process this item
+                ProductionItem productionItem = new(targetItem.ItemName, targetItem.ItemQuantity);
+                NewRecipe? recipe = FindRecipe(targetItem.ItemName);
+                if (recipe != null)
+                {
+                    decimal ratePerMinute = 60M / recipe.ManufactoringDuration;
+                    decimal buildingQuantityRequired = targetItem.ItemQuantity / ratePerMinute;
+                    productionItem.BuildingQuantityRequired = buildingQuantityRequired;
+                }
+                //targetItem.BuildingQuantityRequired = targetItem.Quantity / targetItem.Item.Recipes[0].Outputs[targetItem.Item.Name];
+                //if (targetItem != null && targetItem.Item != null && targetItem.Item.Recipes.Count > 0 && targetItem.Item.Recipes[0].Building != null)
+                //{
+                //    Building? building = FindBuilding(targetItem.Item.Recipes[0].Building);
+                //    if (building != null)
+                //    {
+                //        PowerConsumption += building.PowerConsumption * targetItem.BuildingQuantityRequired;
+                //    }
+                //}
+                //Check if this item is already in the production list, update it instead of adding a new one
+                if (ProductionItems.Any(p => p.Item?.Name == targetItem.Item.Name))
+                {
+                    currentItemMatch = ProductionItems.FirstOrDefault(p => p.Item?.Name == targetItem.Item.Name);
+                    if (currentItemMatch != null)
+                    {
+                        currentItemMatch.Quantity += targetItem.Quantity;
+                        currentItemMatch.BuildingQuantityRequired += targetItem.BuildingQuantityRequired;
+                    }
+                }
+                else
+                {
+                    ProductionItems.Add(targetItem);
+                }
+                decimal itemOutputRatio = targetItem.Quantity / targetItem.Item.Recipes[0].Outputs[targetItem.Item.Name];
+
+                //Process each output (that isn't the target item)
+                foreach (KeyValuePair<string, decimal> output in targetItem.Item.Recipes[0].Outputs)
+                {
+                    //Check for additional outputs
+                    if (output.Key != targetItem.Item.Name)
+                    {
+                        //Process this item
+                        Item? outputItem = FindItem(output.Key);
+                        decimal outputQuantityWithRatio = output.Value * itemOutputRatio;
+                        ProductionItem newProductionItem = new(outputItem, outputQuantityWithRatio)
+                        {
+                            BuildingQuantityRequired = itemOutputRatio
+                        };
+                        if (newProductionItem != null && newProductionItem.Item != null)
+                        {
+                            foreach (KeyValuePair<string, decimal> input in newProductionItem.Item.Recipes[0].Inputs)
+                            {
+                                newProductionItem.Dependencies.Add(input.Key, input.Value);
+                            }
+                            ProductionItems.Add(newProductionItem);
+                            //Commented out this - because we are already adding this input below V
+                            //inputs.AddRange(newProductionItem.Item.Recipes[0].Inputs);
+                        }
+                    }
+                }
+                inputs.AddRange(targetItem.Item.Recipes[0].Inputs);
+
+                //Process each input
+                foreach (KeyValuePair<string, decimal> input in inputs)
+                {
+                    Item? inputItem = FindItem(input.Key);
+                    if (inputItem != null)
+                    {
+                        decimal inputQuantityWithRatio = input.Value * itemOutputRatio;
+                        if (targetItem.Dependencies.Any(p => p.Key == input.Key))
+                        {
+                            KeyValuePair<string, decimal>? currentInputMatch = targetItem.Dependencies.FirstOrDefault(p => p.Key == input.Key);
+                            if (currentInputMatch != null)
+                            {
+                                decimal value = ((KeyValuePair<string, decimal>)currentInputMatch).Value;
+                                currentInputMatch = new KeyValuePair<string, decimal>(input.Key, value + targetItem.Quantity);
+                            }
+                        }
+                        else
+                        {
+                            targetItem.Dependencies.Add(input.Key, inputQuantityWithRatio);
+                        }
+                        ProductionItem newProductionItem = new(inputItem, inputQuantityWithRatio)
+                        {
+                            BuildingQuantityRequired = itemOutputRatio
+                        };
+                        ProcessOutputItem(newProductionItem);
+                    }
+                }
+                //If this item already exists in the production list, update the quantity for it
+                if (currentItemMatch != null)
+                {
+                    foreach (KeyValuePair<string, decimal> dependency in targetItem.Dependencies)
+                    {
+                        if (currentItemMatch.Dependencies.ContainsKey(dependency.Key))
+                        {
+                            currentItemMatch.Dependencies[dependency.Key] += dependency.Value;
+                        }
+                        else
+                        {
+                            currentItemMatch.Dependencies.Add(dependency.Key, dependency.Value);
+                        }
+                    }
+                }
+            }
+            return true;
         }
 
         public NewItem? FindItem(string itemName)
@@ -65,6 +215,54 @@ namespace SatisfactoryTree
             }
             return null;
         }
+
+        public NewBuilding FindBuilding(string buildingName)
+        {
+            foreach (NewBuilding building in Buildings)
+            {
+                if (building.Name == buildingName)
+                {
+                    return building;
+                }
+            }
+            return null;
+        }
+
+        ////Find an item by name
+        //public Item? FindItem(string itemName)
+        //{
+        //    Item? result = null;
+        //    if (Items != null && Items.Count > 0)
+        //    {
+        //        foreach (Item item in Items)
+        //        {
+        //            if (item.Name == itemName)
+        //            {
+        //                result = item;
+        //                break;
+        //            }
+        //        }
+        //    }
+        //    return result;
+        //}
+
+        ////Find a building by name
+        //public Building? FindBuilding(string buildingName)
+        //{
+        //    Building? result = null;
+        //    if (Buildings != null && Buildings.Count > 0)
+        //    {
+        //        foreach (Building item in Buildings)
+        //        {
+        //            if (item.Name == buildingName)
+        //            {
+        //                result = item;
+        //                break;
+        //            }
+        //        }
+        //    }
+        //    return result;
+        //}
 
         ////Build a production plan for a given target item
         //public ProductionCalculation BuildProductionPlan(ProductionItem itemGoal)
