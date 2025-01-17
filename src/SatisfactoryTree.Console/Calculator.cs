e6170abc5a53fae0a511c568fa0fb42bfeec57ea
Sam Smith <samsmithnz@gmail.com>
2025-01-17 08:03:08 -0500

    Added basic calculator

---
+++ src/SatisfactoryTree.Console/Calculator.cs
@@ -0,0 +1,98 @@
+﻿using SatisfactoryTree.Console.OldModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SatisfactoryTree.Console
+{
+    public class Item
+    {
+        public string Name { get; set; }
+        public double Quantity { get; set; }
+        public List<Item> Ingredients { get; set; }
+        public int Counter { get; set; }
+    }
+
+    public class Calculator
+    {
+
+        //Using a target item, calculate the total number of items needed to produce the target item
+        public Calculator() { }
+        public List<Item> CalculateProduction(FinalData finalData, string partName, double quantity)
+        {
+            List<Item> results = new();
+            int counter = 1;
+
+            //Add the goal item
+            results.Add(new() { Name = partName, Quantity = quantity, Ingredients = new(), Counter = counter });
+            //Get the dependencies/ingredients for the goal item
+            results.AddRange(GetIngredients(finalData, partName, quantity, counter));
+
+            //sort the results by counter to show the goal items first and raw items last, and then part name
+            results = results.OrderBy(x => x.Counter).ThenBy(x => x.Name).ToList();
+
+            return results;
+        }
+
+        private List<Item> GetIngredients(FinalData finalData, string partName, double quantity, int counter)
+        {
+            List<Item> results = new();
+            counter++;
+            NewRecipe newRecipe = FindRecipe(finalData, partName);
+
+            //If we have a recipe, calculate the ingredients
+            if (newRecipe != null && newRecipe.products != null)
+            {
+                double ratio = 0;
+                foreach (Product product in newRecipe.products)
+                {
+                    if (product.part == partName)
+                    {
+                        ratio = quantity / product.perMin;
+                        break;
+                    }
+                }
+
+                //If we have a recipe, calculate the ingredients
+                if (newRecipe != null && newRecipe.ingredients != null)
+                {
+                    foreach (Ingredient ingredient in newRecipe.ingredients)
+                    {
+                        //Add this ingredient
+                        results.Add(new() { Name = ingredient.part, Quantity = ingredient.perMin * ratio, Ingredients = new(), Counter = counter });
+                        //Search for this ingredient's dependencies
+                        results.AddRange(GetIngredients(finalData, ingredient.part, ingredient.perMin * ratio, counter));
+
+                    }
+                }
+                ////Check to see if the part is a raw material
+                //else if (finalData.items.rawResources.ContainsKey(partName))
+                //{
+                //    results.Add(new() { Name = partName, Quantity = quantity, Ingredients = new(), Counter = counter });
+                //}
+            }
+            return results;
+        }
+
+        private NewRecipe FindRecipe(FinalData finalData, string partName)
+        {
+            foreach (NewRecipe recipe in finalData.newRecipes)
+            {
+                //Skip alternative recipes for now
+                if (recipe != null && recipe.isAlternate == false && recipe.building.name != "converter")
+                {
+                    foreach (Product product in recipe.products)
+                    {
+                        if (product.part == partName)
+                        {
+                            return recipe;
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ src/SatisfactoryTree.Tests/CalculationTests.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SatisfactoryTree.Console;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SatisfactoryTree.Tests
+{
+    [TestClass]
+    public class CalculationTests
+    {
+        private Console.OldModels.FinalData? finalData = null;
+
+        [TestInitialize]
+        public async Task Initialize()
+        {
+            //arrange
+            Processor processor = new();
+            processor.GetContentFiles();
+            if (processor != null)
+            {
+                string inputFile = processor.InputFile;
+                string outputFile = processor.OutputFile;
+
+                //act
+                finalData = await Processor.ProcessFileOldModel(inputFile, outputFile);
+
+                //assert
+                if (finalData == null)
+                {
+                    Assert.Fail("Final data is null");
+                }
+            }
+        }
+
+        [TestMethod]
+        public void IronIngotsCalculationTest()
+        {
+            //Arrange
+            string partName = "IronIngot";
+            double quantity = 30;
+            if (finalData == null)
+            {
+                Assert.Fail("Final data is null");
+            }
+
+            //Act
+            Calculator calculator = new();
+            List<Item>? results = calculator.CalculateProduction(finalData, partName, quantity);
+
+            //Assert
+            Assert.IsNotNull(results);
+            Assert.AreEqual(2, results.Count);
+            Assert.AreEqual("IronIngot", results[0].Name);
+            Assert.AreEqual(30, results[0].Quantity);
+            Assert.AreEqual(1, results[0].Counter);
+            Assert.AreEqual("OreIron", results[1].Name);
+            Assert.AreEqual(30, results[1].Quantity);
+            Assert.AreEqual(2, results[1].Counter);
+        }
+
+        [TestMethod]
+        public void IronPlatesCalculationTest()
+        {
+            //Arrange
+            string partName = "IronPlate";
+            double quantity = 30;
+            if (finalData == null)
+            {
+                Assert.Fail("Final data is null");
+            }
+
+            //Act
+            Calculator calculator = new();
+            List<Item>? results = calculator.CalculateProduction(finalData, partName, quantity);
+
+            //Assert
+            Assert.IsNotNull(results);
+            Assert.AreEqual(3, results.Count);
+            Assert.AreEqual("IronPlate", results[0].Name);
+            Assert.AreEqual(30, results[0].Quantity);
+            Assert.AreEqual(1, results[0].Counter);
+            Assert.AreEqual("IronIngot", results[1].Name);
+            Assert.AreEqual(45, results[1].Quantity);
+            Assert.AreEqual(2, results[1].Counter);
+            Assert.AreEqual("OreIron", results[2].Name);
+            Assert.AreEqual(45, results[2].Quantity);
+            Assert.AreEqual(3, results[2].Counter);
+        }
+    }
+}
